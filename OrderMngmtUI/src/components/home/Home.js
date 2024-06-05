cdf212ff4a3243d8b2eb8945f186a033bee57e31
yiyilopez <146986398+yiyilopez@users.noreply.github.com>
2024-06-05 13:16:45 -0500

    modified admin page to add users and comments

--- OrderMngmtUI/src/components/home/Home.js
+++ OrderMngmtUI/src/components/home/Home.js
@@ -1,34 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Statistic, Icon, Grid, Container, Image, Segment, Dimmer, Loader } from 'semantic-ui-react';
-import { orderApi } from '../misc/OrderApi';
-import { handleLogError } from '../misc/Helpers';
+import { Grid, Container, Segment, Dimmer, Loader, Image } from 'semantic-ui-react';
 import image1 from '../../assets/imagen1.png';
 import image2 from '../../assets/imagen2.png';
 
 function Home() {
-  const [numberOfUsers, setNumberOfUsers] = useState(0);
-  const [numberOfOrders, setNumberOfOrders] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const responseUsers = await orderApi.numberOfUsers();
-        const numberOfUsers = responseUsers.data;
-
-        const responseOrders = await orderApi.numberOfOrders();
-        const numberOfOrders = responseOrders.data;
-
-        setNumberOfUsers(numberOfUsers);
-        setNumberOfOrders(numberOfOrders);
-      } catch (error) {
-        handleLogError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchData();
+    // Simulate a delay to demonstrate loading state
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
   }, []);
 
   const noticias = [
@@ -54,34 +36,12 @@ function Home() {
 
   return (
     <Container text>
-      <Grid stackable columns={2}>
-        <Grid.Row>
-          <Grid.Column textAlign='center'>
-            <Segment color='teal'>
-              <Statistic>
-                <Statistic.Value><Icon name='user' color='grey' />{numberOfUsers}</Statistic.Value>
-                <Statistic.Label>Users</Statistic.Label>
-              </Statistic>
-            </Segment>
-          </Grid.Column>
-          <Grid.Column textAlign='center'>
-            <Segment color='teal'>
-              <Statistic>
-                <Statistic.Value><Icon name='laptop' color='grey' />{numberOfOrders}</Statistic.Value>
-                <Statistic.Label>Orders</Statistic.Label>
-              </Statistic>
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-      
       {noticias.map((noticia, index) => (
         <div key={index} style={{ marginTop: '2em', textAlign: 'center' }}>
           <Image src={noticia.image} alt={noticia.title} />
           <h3>{noticia.title}</h3>
         </div>
       ))}
-
     </Container>
   );
 }
